f4bab86dec83838065a49ebd82d7d01aecec7ab1
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-02-25 15:02:42 +0100

    fix(base) Move Tristate constexpr operators in header

--- src/Tristate.cpp
+++ src/Tristate.cpp
@@ -11,41 +11,13 @@
 
 using namespace nts;
 
-constexpr Tristate operator&(Tristate a, Tristate b)
-{
-    if (a == False || b == False)
-        return False;
-    return a == Undefined || b == Undefined ? Undefined : True;
-}
-
-constexpr Tristate operator|(Tristate a, Tristate b)
-{
-    if (a == True || b == True)
-        return True;
-    return a == Undefined || b == Undefined ? Undefined : False;
-}
-
-constexpr Tristate operator^(Tristate a, Tristate b)
-{
-    if (a == Undefined || b == Undefined)
-        return Undefined;
-    return a != b ? True : False;
-}
-
-constexpr Tristate operator!(Tristate a)
-{
-    if (a == Undefined)
-        return Undefined;
-    return a == True ? False : True;
-}
-
 static const std::map<Tristate, char> _tristateCharacters = {
     {Undefined, 'U'},
     {False, '0'},
     {True, '1'}
 };
 
-std::ostream &operator<<(std::ostream &os, Tristate state)
+std::ostream &nts::operator<<(std::ostream &os, Tristate state)
 {
     return os << _tristateCharacters.at(state);
 }
--- src/Tristate.hpp
+++ src/Tristate.hpp
@@ -17,11 +17,36 @@ namespace nts {
         False = false
     };
 
-    constexpr Tristate operator&(Tristate a, Tristate b);
-    constexpr Tristate operator^(Tristate a, Tristate b);
-    constexpr Tristate operator|(Tristate a, Tristate b);
 
-    constexpr Tristate operator!(Tristate a);
+    constexpr Tristate operator&(Tristate a, Tristate b)
+    {
+        if (a == False || b == False)
+            return False;
+        return a == Undefined || b == Undefined ? Undefined : True;
+    }
+
+    constexpr Tristate operator|(Tristate a, Tristate b)
+    {
+        if (a == True || b == True)
+            return True;
+        return a == Undefined || b == Undefined ? Undefined : False;
+    }
+
+    constexpr Tristate operator^(Tristate a, Tristate b)
+    {
+        if (a == Undefined || b == Undefined)
+            return Undefined;
+        return a != b ? True : False;
+    }
+
+
+    constexpr Tristate operator!(Tristate a)
+    {
+        if (a == Undefined)
+            return Undefined;
+        return a == True ? False : True;
+    }
+
 
     std::ostream &operator<<(std::ostream &os, Tristate state);
 }
