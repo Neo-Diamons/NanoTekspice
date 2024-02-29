0df0e40f4ca9bda3f3654a971befa751c0ce0bbc
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-02-29 02:54:32 +0100

    fix(components) Remove old conflicting definitions from orginal IComponent.hpp

--- src/IComponent.hpp
+++ src/IComponent.hpp
@@ -12,92 +12,9 @@
 #include <map>
 
 #include "Tristate.hpp"
-#include "components/IComponent.hpp"
+#include "components/AComponent.hpp"
 
 namespace nts {
-    class Link {
-    public:
-        Link() : _pin(0)
-        {};
-
-        Link(const std::shared_ptr<IComponent> &component, std::size_t pin)
-            : _component(component), _pin(pin)
-        {};
-
-        [[nodiscard]] nts::Tristate compute() const
-        {
-            return _component->compute(this->_pin);
-        }
-
-    private:
-        std::shared_ptr<IComponent> _component;
-        std::size_t _pin;
-    };
-
-    // Abstract
-    class AComponent : public IComponent {
-    protected:
-        std::map<size_t, Link> _pins{};
-
-    public:
-        class Exception : public std::exception {
-        private:
-            std::string _message;
-
-        public:
-            explicit Exception(std::string message);
-
-            [[nodiscard]] const char *what() const noexcept override;
-        };
-
-        void setLink(std::size_t pin, std::shared_ptr<IComponent> other, std::size_t otherPin) override;
-    };
-
-    class ValueComponent : public AComponent {
-    public:
-        void setLink([[maybe_unused]] std::size_t pin, [[maybe_unused]] std::shared_ptr<IComponent> other,
-            [[maybe_unused]] std::size_t otherPin) final
-        {};
-    };
-
-    // Special components
-    class InputComponent : public AComponent {
-    private:
-        nts::Tristate _state = nts::Undefined;
-        nts::Tristate _oldState = nts::Undefined;
-
-    public:
-
-        nts::Tristate compute(std::size_t pin) override;
-
-        void setState(nts::Tristate state);
-    };
-
-    class OutputComponent : public AComponent {
-    public:
-
-        nts::Tristate compute(std::size_t pin) override;
-    };
-
-    class TrueComponent : public ValueComponent {
-    public:
-        nts::Tristate compute(std::size_t pin) override;
-    };
-
-    class FalseComponent : public ValueComponent {
-    public:
-        nts::Tristate compute(std::size_t pin) override;
-    };
-
-    class ClockComponent : public ValueComponent {
-    private:
-        nts::Tristate _state = nts::Undefined;
-
-    public:
-
-        nts::Tristate compute(std::size_t pin) override;
-    };
-
     // Elementary components
     class AndComponent : public AComponent {
     public:
@@ -153,8 +70,6 @@ namespace nts {
     // Advanced components
     class Gates4008 : public AComponent {
     private:
-        std::map<int, nts::Tristate> _outPins;
-
         nts::Tristate add(int idA, int idB, nts::Tristate carry);
 
     public:
